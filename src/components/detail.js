60673dcb9ec2b77ee16db400e09b5d5637c94f39
Shadab Jabbar <shadabjabbar@Shadabs-MacBook-Air.local>
2018-12-08 21:24:49 +0530

    detail page

--- src/components/detail.js
+++ src/components/detail.js
@@ -1,9 +1,49 @@
 import React, { Component } from "react";
 import "./detail.css";
 
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
 class Detail extends Component {
   render() {
-    return <div className="App">{this.props.user.name}</div>;
+    return (
+      <div className="detail-App">
+        <ExpansionPanel defaultExpanded>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            style={{ backgroundColor: "#e5e6e8" }}
+          >
+            Basic Details
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails
+            style={{ padding: "10px", display: "inline-block", width: "100%" }}
+          >
+            <div className="field">
+              <div className="label">Name : </div>
+              <div className="data">{this.props.user.name}</div>
+            </div>
+            <br />
+            <div className="field">
+              <div className="label">Phone : </div>
+              <div className="data">{this.props.user.phone}</div>
+            </div>
+            <br />
+            <div className="field">
+              <div className="label">Email : </div>
+              <div className="data">{this.props.user.email}</div>
+            </div>
+            <br />
+            <div className="field">
+              <div className="label">Website : </div>
+              <div className="data">{this.props.user.website}</div>
+            </div>
+            <br />
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </div>
+    );
   }
 }
 
